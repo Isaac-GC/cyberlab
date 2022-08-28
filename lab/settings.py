9547af7483c53f385f34b179c999fddf86fe2cf2
Isaac Gray-Christensen <christensenisaac1995@gmail.com>
2022-08-28 17:21:59 -0600

    Added profile

--- lab/settings.py
+++ lab/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "channels",
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ STATIC_ROOT = STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ASGI_APPLICATION = 'lab.asgi.application'
