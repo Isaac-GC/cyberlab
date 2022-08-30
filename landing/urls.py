3f57d993da2b2dc2ec891426fbfd1d8711d63f2f
Isaac <christensenisaac1995@gmail.com>
2022-08-29 21:09:59 -0600

    Updated and created settings page

--- landing/urls.py
+++ landing/urls.py
@@ -7,4 +7,5 @@ app_name = "landing"
 urlpatterns = [
     path('', views.index, name='index'),
     path('home', views.home, name='home'),
+    path('settings', views.settings, name='settings'),
 ]
--- landing/views.py
+++ landing/views.py
@@ -7,3 +7,6 @@ def index(request):
 
 def home(request):
     return render(request, 'home.html')
+
+def settings(request):
+    return render(request, 'settings.html')
